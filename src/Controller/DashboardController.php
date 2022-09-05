a5824c7257265d450d680e16fac51b1b8d83d9dd
Steven Maitre <73316151+StevenM91@users.noreply.github.com>
2022-09-05 11:19:14 +0200

    modification

--- src/Controller/DashboardController.php
+++ src/Controller/DashboardController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use App\Entity\User;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -17,7 +18,7 @@ class DashboardController extends AbstractController
     }
 
     /**
-     * @Route("/user/dashboard", name="app_dashboard")
+     * @Route("/admin/dashboard", name="app_dashboard")
      */
     public function index(): Response
     {
@@ -31,4 +32,47 @@ class DashboardController extends AbstractController
             "users" => $infoUser,
         ]);
     }
+
+    /**
+     * @Route("admin/dashboard/remove/{id}", name="app_remove_user")
+     */
+    // public function deleteUser($id)
+    // {
+    //     $singleUser = $this->manager->getRepository(User::class)->findBy(['id' => $id]);
+
+    //     $this->manager->remove($singleUser[0]);
+    //     $this->manager->flush();
+
+    //     return $this->redirectToRoute('app_dashboard');
+    // }
+
+
+    // public function updateArticle(Request $request, $id)
+    // {
+
+    //     // Je récupere dans la bdd 1 article grâce à l'id
+    //     $singleUser = $this->manager->getRepository(Article::class)->findBy(['id' => $id]);
+
+
+    //     $form = $this->createForm(RegisterType::class, $singleUser[0]);
+    //     $form->handleRequest($request);
+
+
+    //     // Si le formulaire et soumis et en meme temp valide alors j'envoi les modification en bdd
+    //     if ($form->isSubmitted() && $form->isValid()) {
+
+
+    //         $this->manager->persist($singleUser[0]);
+    //         $this->manager->flush();
+
+    //         return $this->redirectToRoute('app_home');
+    //     }
+
+
+
+    //     return $this->render('article/update.html.twig', [
+    //         'form' => $form->createView(),
+    //         'singleArticle' => $singleArticle[0]
+    //     ]);
+    // }
 }
